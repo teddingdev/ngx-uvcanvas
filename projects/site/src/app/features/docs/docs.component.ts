edac6cfc2254a86b86f0e62668ced9245973529a
teddingdev <teddingdev@gmail.com>
2024-04-10 18:03:38 +0800

    feat: update docs

--- projects/site/src/app/features/docs/docs.component.ts
+++ projects/site/src/app/features/docs/docs.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { BreakpointService } from '@services/breakpoint.service';
 import { provideMarkdown } from 'ngx-markdown';
 
 @Component({
@@ -13,7 +14,33 @@ export class DocsComponent {
   get url() {
     return this.router.url;
   }
-  constructor(private router: Router) {
-    console.log(this.router);
+
+  currentScreenSize:
+    | 'Unknown'
+    | 'XSmall'
+    | 'Small'
+    | 'Medium'
+    | 'Large'
+    | 'XLarge' = 'Unknown';
+
+  menuDefaultOpened = new Map([
+    ['Unknown', false],
+    ['XSmall', false],
+    ['Small', false],
+    ['Medium', false],
+    ['Large', true],
+    ['XLarge', true],
+  ]);
+
+  menuOpened = true;
+
+  constructor(
+    private router: Router,
+    private breakpointService: BreakpointService
+  ) {
+    this.breakpointService.currentScreenSize$.subscribe((currentScreenSize) => {
+      this.currentScreenSize = currentScreenSize;
+      this.menuOpened = this.menuDefaultOpened.get(currentScreenSize) || false;
+    });
   }
 }
